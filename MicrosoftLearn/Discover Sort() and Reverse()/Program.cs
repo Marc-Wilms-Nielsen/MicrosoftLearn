2905cce9f3c5a2b292e21d53fdaac67cd9874710
Marc Nielsen <marc-wilms@hotmail.com>
2025-08-27 15:16:51 +0200

    Sort and Reverse

---
+++ MicrosoftLearn/Discover Sort() and Reverse()/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+/*
+
+string[] pallets = ["B12", "A11", "B12", "A12"];
+
+
+//sort alphabetically.
+
+Console.WriteLine("sorted...");
+Array.Sort(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"--{pallet}");
+}
+
+
+//Reverse the order of the pallets
+
+Console.WriteLine("");
+Console.WriteLine("Reversed...");
+Array.Reverse(pallets);
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+
+//clear and resize an array
+//when u clear a value it becomes null, so i has no value type.
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+*/
+
+
+string[] pallets =  ["B14", "A11", "B12", "A13" ];
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
