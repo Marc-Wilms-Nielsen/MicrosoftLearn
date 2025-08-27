bf09d31999ca7283bc08c4f426cd955ac8434542
Marc Nielsen <marc-wilms@hotmail.com>
2025-08-27 16:07:21 +0200

    update new module done

--- MicrosoftLearn/Discover Sort() and Reverse()/Program.cs
+++ MicrosoftLearn/Discover Sort() and Reverse()/Program.cs
@@ -38,7 +38,7 @@ foreach (var pallet in pallets)
 */
 
 
-string[] pallets =  ["B14", "A11", "B12", "A13" ];
+string[] pallets = [ "B14", "A11", "B12", "A13" ];
 Console.WriteLine("");
 
 Array.Clear(pallets, 0, 2);
@@ -54,6 +54,15 @@ Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
 
 pallets[4] = "C01";
 pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
 
 foreach (var pallet in pallets)
 {
